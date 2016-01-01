dd2499e4184b578650e1bcea4ce19974b117e533
Yathannsh <yathannsh@gmail.com>
2016-01-01 15:15:34 +0530

    Initial commit

---
+++ gci.py
@@ -0,0 +1,7 @@
+age = int(input('How old are you?'))
+if (age > 13 & age < 17):
+  print('Google Code-in is the place for you!')
+elif(age<13):
+  print('Sorry,you are young to participate in any Google contest.')
+else:
+  print('Google Summer of Code awaits you!')
